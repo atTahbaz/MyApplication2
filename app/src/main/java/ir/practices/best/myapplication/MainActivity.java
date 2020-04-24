4c965d7964cf71b5e409d52ea1f439bbc8532477
Ate <ate.tahbaz@gmail.com>
2020-04-24 17:46:42 +0430

    just a test for git session

--- app/src/main/java/ir/practices/best/myapplication/MainActivity.java
+++ app/src/main/java/ir/practices/best/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package ir.practices.best.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
